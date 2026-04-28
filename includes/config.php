7585113e3383a418ced0b6a0d80fb5dd032e24a2
Daniel K Yantis <info@DanielKYantis.me>
2026-04-28 05:49:43 -0500

    Add canonical business address constants

--- includes/config.php
+++ includes/config.php
@@ -6,9 +6,12 @@ const BUSINESS_NAME = 'Mark’s Services LLC';
 const BUSINESS_EMAIL = 'info@MarksServices.com';
 const BUSINESS_PHONE_DISPLAY = '(512) 549-0322';
 const BUSINESS_PHONE_TEL = '+15125490322';
+const BUSINESS_STREET = '25 Waters Edge Circle';
 const BUSINESS_CITY = 'Georgetown';
 const BUSINESS_STATE = 'TX';
 const BUSINESS_ZIP = '78626';
+const BUSINESS_ADDRESS_DISPLAY = BUSINESS_STREET . ', ' . BUSINESS_CITY . ', ' . BUSINESS_STATE . ' ' . BUSINESS_ZIP;
+const BUSINESS_AREA = 'Georgetown & Sun City, Texas';
 const ELECTRICAL_LICENSE = 'TECL 20547';
 const PLUMBING_LICENSE = 'M-38601';
 
